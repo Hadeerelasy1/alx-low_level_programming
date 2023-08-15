2a7cabb5b8140689d2b9119318162e94dd30f7b0
Hadeerelasy1 <hadeerelasy1@gmail.com>
2023-08-15 04:59:44 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,9 @@
  * Return: Always 0 (success)
 */
 int main(void)
-	
-	char str[] ="_putchar":
-	int ch:
-
-	for (ch =0; ch <8; ch++)
-	        _putchar(str[ch]):
+	char str[] = "_putchar";
+	int ch;
+	for (ch = 0; ch < 8; ch++);
+	        _putchar(str[ch];
 	_putchar("\n");
-
 	return (0)
