35d4e96ac9c7bcf877f950bf6084de221d605f4b
Hadeerelasy1 <hadeerelasy1@gmail.com>
2023-08-15 13:35:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char n = 'a';
+
+	do {
+		putchar(n);
+		n++;
+	} while (n != 123);
+	printf("\n");
+	return (0);
+}
